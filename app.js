c31d57bd5f46ba962d20f26bb2fb67ce247f668a
Anupriya KV <anupriyakrishanan03@gmail.com>
2025-03-21 14:25:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const evn = require("dotenv").config();
+const db = require("./config/db")
+db()
+app.set
+
+const PORT = process.env.PORT || 3000; // Change to any available port
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
