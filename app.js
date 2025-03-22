cf4d28e5b57a521bac40a9797824184c40f99efa
Anupriya KV <anupriyakrishanan03@gmail.com>
2025-03-22 11:36:15 +0530

    Added missing files

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 const evn = require("dotenv").config();
 const db = require("./config/db")
 db()
-app.set
+const userRouter = require("./routes/userRouter")
 
-const PORT = process.env.PORT || 3000; // Change to any available port
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.set("view engine","ejs")
+app.set("views", [path.join(__dirname, 'views/user'), path.join(__dirname, 'views/admin')]);
+app.use(express.static(path.join(__dirname,"public")))
+
+
+app.use("/",userRouter)
+
+ // Change to any available port
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port");
 });
 
 module.exports = app;
 
-
-console.log('Name')
